941c682998d4e8f2d1f4cdb327cbb50b8ec77d0b
RamG007 <48403226+RamG007@users.noreply.github.com>
2021-04-05 11:38:01 +0530

    Profile UI

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FileChooser } from '@ionic-native/file-chooser/ngx';
+import { FilePath } from '@ionic-native/file-path/ngx';
 
 @Component({
   selector: 'app-tab3',
@@ -7,8 +9,22 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  profilePic = "../../assets/profile.jpg";
+  profilePic = "https://cultivatedculture.com/wp-content/uploads/2019/12/LinkedIn-Profile-Picture-Example-Madeline-Mann.jpeg";
+  //profilePic = null;
+  profilePicPath:string = null;
 
-  constructor() {}
+  constructor(
+    private fileChooser: FileChooser,
+    private filePath: FilePath,
+  ) {}
+
+  SelectFile() {
+    let filter = {"mime": "audio/*"};
+    this.fileChooser.open(filter).then((fileuri) => {
+      this.filePath.resolveNativePath(fileuri).then((resolvedPath) => {
+        this.profilePicPath = resolvedPath;
+      });
+    });
+  }
 
 }
